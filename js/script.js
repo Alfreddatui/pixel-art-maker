6fba0e22b7b50e6508f9e6c23722c1a0aa38f693
Alfred Datui <alfred@autoarmour.co>
2017-11-10 00:39:04 +0800

    Add the second solution and some suggestion

--- js/script.js
+++ js/script.js
@@ -1,18 +1,10 @@
-$(document).ready(jQueryTotal);
-
-function jQueryTotal() {
-  makeGrid();
-  changeColor();
-}
-
 const makeGrid = () => { // Function to create the Grid
   $('#sizePicker').submit(function(e) {
     e.preventDefault(); // To make the html page not reload if you submit the form
-    let height, width;
-    height = $('#input_height').val();
-    width = $('#input_width').val();
+    let height = $('#input_height').val();
+    let width = $('#input_width').val();
     let content = '';
-  
+
     for (let row = 0; row < height; row++) { //nested for loop to create the Grid by changing the text inside table in the html file
       content += '<tr>';
       for (let col = 0; col < width; col++) {
@@ -21,6 +13,22 @@ const makeGrid = () => { // Function to create the Grid
       content += '</tr>';
     }
     $('#pixel_canvas').html(content);
+
+    /////////////////////Second solution///////////////////////////
+    // const table = document.getElementById('pixel_canvas');
+    // table.innerHTML = "";
+    // for (let i = 0; i < height; i++) {
+    //     // Inserts 10 rows into the table
+    //     const row = table.insertRow(i);
+    //     for (let j = 0; j < width; j++) {
+    //         // Inserts 10 cells into each of the rows
+    //         const cell = row.insertCell(j);
+    //         cell.addEventListener('click', function() {
+    //           let color = $('#colorPicker').val();
+    //           cell.style.backgroundColor = color;
+    //         });
+    //     }
+    // }
   });
 };
 
@@ -28,8 +36,9 @@ const changeColor = () => { //change the color + add the feature where you can p
   $('#pixel_canvas').click(function(e) {
     let box = e.target;
     // console.log(e.target);
-    if (e.target != 'table') { //To avoid if you accidentally click the table part, so it wont change the background color entirely
-       let color = $('#colorPicker').val();
+    console.log(box.nodeName);
+    if (box.nodeName != 'TABLE' && box.nodeName != 'TR') { //To avoid if you accidentally click the table part, so it wont change the background color entirely
+      let color = $('#colorPicker').val();
       if(event.altKey || event.metaKey) { //metaKey is command key for mac
         $(box).css("background-color", "rgba(0, 0, 0, 0)");
       } else {
@@ -38,3 +47,7 @@ const changeColor = () => { //change the color + add the feature where you can p
     }
   })
 };
+
+makeGrid();
+changeColor();
+
